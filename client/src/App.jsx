0ed84887975a3d3092fdef873a5953f6df834ca5
Vichel <2117236210@qq.com>
2025-08-17 14:47:21 +0800

    1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,51 @@
 function App() {
   return (
-    <h1>Welcome to the Student System</h1>
+    // 最外层容器
+    <div id="app" className="container">
+      {/* 顶部导航栏 */}
+      <nav className="navbar navbar-inverse navbar-fixed-top">
+        <div className="container">
+          <div className="navbar-header">
+            <button
+              type="button"
+              className="navbar-toggle collapsed"
+              data-toggle="collapse"
+              data-target="#navbar"
+              aria-expanded="false"
+              aria-controls="navbar"
+            >
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="#">
+              Project name
+            </a>
+          </div>
+          <div id="navbar" className="collapse navbar-collapse">
+            <ul className="nav navbar-nav">
+              <li className="active">
+                <a href="#">主页</a>
+              </li>
+              <li>
+                <a href="#">关于我们</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+            <ul className="nav navbar-nav navbar-right">
+              <li>
+                <a href="#">添加用户</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      {/* 匹配上的路由显示在这个位置 */}
+
+    </div>
   );
 }
 
